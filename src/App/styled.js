57bd459daf87ffc2ce1d07150c164e623dcc1914
Michal Szustkiewicz <szustka755@gmail.com>
2023-06-01 12:49:53 +0200

    changed styles

--- src/App/styled.js
+++ src/App/styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-basis: 650px;
     margin: 20px;
-    padding: 40px;
+    padding: 25px;
+    width: 700px;
+    min-height: 400px;
     background: ${({ theme }) => theme.color.gallery};
     border-radius: 15px;
     box-shadow: 5px 5px 30px black;
