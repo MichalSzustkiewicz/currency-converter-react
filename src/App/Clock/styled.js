1e30104c6d8358465276950ce4f063498e8b3b67
Michal Szustkiewicz <szustka755@gmail.com>
2023-06-01 12:56:57 +0200

    clock styles modified

--- src/App/Clock/styled.js
+++ src/App/Clock/styled.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const StyledClock = styled.p`
-    display: flex;
-    justify-content: flex-end;
     font-family: monospace;
     margin: 0;
     padding-bottom: 10px;
+    text-align: right;
 `; 
