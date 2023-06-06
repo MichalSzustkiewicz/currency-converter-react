af8161c7b9691f6894037f9168d8ce4bb0130f98
Michal Szustkiewicz <szustka755@gmail.com>
2023-06-06 13:29:24 +0200

    url address declaration removed from useRatesData

--- src/App/useRatesData.js
+++ src/App/useRatesData.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const requestURL = "https://api.exchangerate.host/latest?base=PLN&symbols=AUD,CAD,CHF,EUR,GBP,ILS,USD";
+
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState({
         status: "loading",
@@ -9,7 +11,6 @@ export const useRatesData = () => {
     useEffect(() => {
         const fetchRates = async () => {
             try {
-                const requestURL = "https://api.exchangerate.host/latest?base=PLN&symbols=AUD,CAD,CHF,EUR,GBP,ILS,USD";
                 const response = await axios.get(requestURL);
                 const { rates, date } = response.data;
 
